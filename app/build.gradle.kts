22a9c791a1c91a97ae35fec570423888a5f4d5a8
Nicolas Queijo <queijonicolas@gmail.com>
2024-05-01 14:45:19 -0400

    Sorted dependencies alphabetically.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
