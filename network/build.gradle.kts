8f3d4233a0acdd70ec9dedd37ca2f8611e9fbc26
Nicolas Queijo <queijonicolas@gmail.com>
2024-05-03 12:50:22 -0400

    Added retrofit dependency to network module.

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -33,10 +33,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
