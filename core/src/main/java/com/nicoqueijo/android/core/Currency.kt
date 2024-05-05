7c8995c9a55b359d5195d15ecaa391a7f646bbff
Nicolas Queijo <queijonicolas@gmail.com>
2024-05-04 22:13:47 -0400

    Kdoc Currency data class.

--- core/src/main/java/com/nicoqueijo/android/core/Currency.kt
+++ core/src/main/java/com/nicoqueijo/android/core/Currency.kt
@@ -1,5 +1,11 @@
 package com.nicoqueijo.android.core
 
+/**
+ * Represents a currency with its code and exchange rate.
+ *
+ * @property currencyCode The three-letter ISO 4217 currency code.
+ * @property exchangeRate The exchange rate of this currency to one US Dollar (USD).
+ */
 data class Currency(
     val currencyCode: String,
     val exchangeRate: Double
