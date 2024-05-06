c8af28507ada7853956a8aad863d2dbe37fbe13a
Nicolas Queijo <queijonicolas@gmail.com>
2024-05-05 23:41:47 -0400

    Converted network model classes to data classes.

--- network/src/main/java/com/nicoqueijo/android/network/ApiEndPoint.kt
+++ network/src/main/java/com/nicoqueijo/android/network/ApiEndPoint.kt
@@ -7,9 +7,8 @@ import kotlinx.serialization.Serializable
  * https://docs.openexchangerates.org/docs/latest-json
  */
 @Serializable
-class ApiEndPoint {
-    var timestamp: Long = 0L
-
+data class ApiEndPoint(
+    var timestamp: Long = 0L,
     @SerialName("rates")
     var exchangeRates: ExchangeRates? = null
-}
+)
--- network/src/main/java/com/nicoqueijo/android/network/ExchangeRates.kt
+++ network/src/main/java/com/nicoqueijo/android/network/ExchangeRates.kt
@@ -5,7 +5,514 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-class ExchangeRates {
+data class ExchangeRates(
+    @SerialName("AED")
+    var USD_AED: Double = 0.0,
+
+    @SerialName("AFN")
+    var USD_AFN: Double = 0.0,
+
+    @SerialName("ALL")
+    var USD_ALL: Double = 0.0,
+
+    @SerialName("AMD")
+    var USD_AMD: Double = 0.0,
+
+    @SerialName("ANG")
+    var USD_ANG: Double = 0.0,
+
+    @SerialName("AOA")
+    var USD_AOA: Double = 0.0,
+
+    @SerialName("ARS")
+    var USD_ARS: Double = 0.0,
+
+    @SerialName("AUD")
+    var USD_AUD: Double = 0.0,
+
+    @SerialName("AWG")
+    var USD_AWG: Double = 0.0,
+
+    @SerialName("AZN")
+    var USD_AZN: Double = 0.0,
+
+    @SerialName("BAM")
+    var USD_BAM: Double = 0.0,
+
+    @SerialName("BBD")
+    var USD_BBD: Double = 0.0,
+
+    @SerialName("BDT")
+    var USD_BDT: Double = 0.0,
+
+    @SerialName("BGN")
+    var USD_BGN: Double = 0.0,
+
+    @SerialName("BHD")
+    var USD_BHD: Double = 0.0,
+
+    @SerialName("BIF")
+    var USD_BIF: Double = 0.0,
+
+    @SerialName("BMD")
+    var USD_BMD: Double = 0.0,
+
+    @SerialName("BND")
+    var USD_BND: Double = 0.0,
+
+    @SerialName("BOB")
+    var USD_BOB: Double = 0.0,
+
+    @SerialName("BRL")
+    var USD_BRL: Double = 0.0,
+
+    @SerialName("BSD")
+    var USD_BSD: Double = 0.0,
+
+    @SerialName("BTC")
+    var USD_BTC: Double = 0.0,
+
+    @SerialName("BTN")
+    var USD_BTN: Double = 0.0,
+
+    @SerialName("BWP")
+    var USD_BWP: Double = 0.0,
+
+    @SerialName("BYN")
+    var USD_BYN: Double = 0.0,
+
+    @SerialName("BZD")
+    var USD_BZD: Double = 0.0,
+
+    @SerialName("CAD")
+    var USD_CAD: Double = 0.0,
+
+    @SerialName("CDF")
+    var USD_CDF: Double = 0.0,
+
+    @SerialName("CHF")
+    var USD_CHF: Double = 0.0,
+
+    @SerialName("CLF")
+    var USD_CLF: Double = 0.0,
+
+    @SerialName("CLP")
+    var USD_CLP: Double = 0.0,
+
+    @SerialName("CNH")
+    var USD_CNH: Double = 0.0,
+
+    @SerialName("CNY")
+    var USD_CNY: Double = 0.0,
+
+    @SerialName("COP")
+    var USD_COP: Double = 0.0,
+
+    @SerialName("CRC")
+    var USD_CRC: Double = 0.0,
+
+    @SerialName("CUC")
+    var USD_CUC: Double = 0.0,
+
+    @SerialName("CUP")
+    var USD_CUP: Double = 0.0,
+
+    @SerialName("CVE")
+    var USD_CVE: Double = 0.0,
+
+    @SerialName("CZK")
+    var USD_CZK: Double = 0.0,
+
+    @SerialName("DJF")
+    var USD_DJF: Double = 0.0,
+
+    @SerialName("DKK")
+    var USD_DKK: Double = 0.0,
+
+    @SerialName("DOP")
+    var USD_DOP: Double = 0.0,
+
+    @SerialName("DZD")
+    var USD_DZD: Double = 0.0,
+
+    @SerialName("EGP")
+    var USD_EGP: Double = 0.0,
+
+    @SerialName("ERN")
+    var USD_ERN: Double = 0.0,
+
+    @SerialName("ETB")
+    var USD_ETB: Double = 0.0,
+
+    @SerialName("EUR")
+    var USD_EUR: Double = 0.0,
+
+    @SerialName("FJD")
+    var USD_FJD: Double = 0.0,
+
+    @SerialName("FKP")
+    var USD_FKP: Double = 0.0,
+
+    @SerialName("GBP")
+    var USD_GBP: Double = 0.0,
+
+    @SerialName("GEL")
+    var USD_GEL: Double = 0.0,
+
+    @SerialName("GGP")
+    var USD_GGP: Double = 0.0,
+
+    @SerialName("GHS")
+    var USD_GHS: Double = 0.0,
+
+    @SerialName("GIP")
+    var USD_GIP: Double = 0.0,
+
+    @SerialName("GMD")
+    var USD_GMD: Double = 0.0,
+
+    @SerialName("GNF")
+    var USD_GNF: Double = 0.0,
+
+    @SerialName("GTQ")
+    var USD_GTQ: Double = 0.0,
+
+    @SerialName("GYD")
+    var USD_GYD: Double = 0.0,
+
+    @SerialName("HKD")
+    var USD_HKD: Double = 0.0,
+
+    @SerialName("HNL")
+    var USD_HNL: Double = 0.0,
+
+    @SerialName("HRK")
+    var USD_HRK: Double = 0.0,
+
+    @SerialName("HTG")
+    var USD_HTG: Double = 0.0,
+
+    @SerialName("HUF")
+    var USD_HUF: Double = 0.0,
+
+    @SerialName("IDR")
+    var USD_IDR: Double = 0.0,
+
+    @SerialName("ILS")
+    var USD_ILS: Double = 0.0,
+
+    @SerialName("IMP")
+    var USD_IMP: Double = 0.0,
+
+    @SerialName("INR")
+    var USD_INR: Double = 0.0,
+
+    @SerialName("IQD")
+    var USD_IQD: Double = 0.0,
+
+    @SerialName("IRR")
+    var USD_IRR: Double = 0.0,
+
+    @SerialName("ISK")
+    var USD_ISK: Double = 0.0,
+
+    @SerialName("JEP")
+    var USD_JEP: Double = 0.0,
+
+    @SerialName("JMD")
+    var USD_JMD: Double = 0.0,
+
+    @SerialName("JOD")
+    var USD_JOD: Double = 0.0,
+
+    @SerialName("JPY")
+    var USD_JPY: Double = 0.0,
+
+    @SerialName("KES")
+    var USD_KES: Double = 0.0,
+
+    @SerialName("KGS")
+    var USD_KGS: Double = 0.0,
+
+    @SerialName("KHR")
+    var USD_KHR: Double = 0.0,
+
+    @SerialName("KMF")
+    var USD_KMF: Double = 0.0,
+
+    @SerialName("KPW")
+    var USD_KPW: Double = 0.0,
+
+    @SerialName("KRW")
+    var USD_KRW: Double = 0.0,
+
+    @SerialName("KWD")
+    var USD_KWD: Double = 0.0,
+
+    @SerialName("KYD")
+    var USD_KYD: Double = 0.0,
+
+    @SerialName("KZT")
+    var USD_KZT: Double = 0.0,
+
+    @SerialName("LAK")
+    var USD_LAK: Double = 0.0,
+
+    @SerialName("LBP")
+    var USD_LBP: Double = 0.0,
+
+    @SerialName("LKR")
+    var USD_LKR: Double = 0.0,
+
+    @SerialName("LRD")
+    var USD_LRD: Double = 0.0,
+
+    @SerialName("LSL")
+    var USD_LSL: Double = 0.0,
+
+    @SerialName("LYD")
+    var USD_LYD: Double = 0.0,
+
+    @SerialName("MAD")
+    var USD_MAD: Double = 0.0,
+
+    @SerialName("MDL")
+    var USD_MDL: Double = 0.0,
+
+    @SerialName("MGA")
+    var USD_MGA: Double = 0.0,
+
+    @SerialName("MKD")
+    var USD_MKD: Double = 0.0,
+
+    @SerialName("MMK")
+    var USD_MMK: Double = 0.0,
+
+    @SerialName("MNT")
+    var USD_MNT: Double = 0.0,
+
+    @SerialName("MOP")
+    var USD_MOP: Double = 0.0,
+
+    @SerialName("MRU")
+    var USD_MRU: Double = 0.0,
+
+    @SerialName("MUR")
+    var USD_MUR: Double = 0.0,
+
+    @SerialName("MVR")
+    var USD_MVR: Double = 0.0,
+
+    @SerialName("MWK")
+    var USD_MWK: Double = 0.0,
+
+    @SerialName("MXN")
+    var USD_MXN: Double = 0.0,
+
+    @SerialName("MYR")
+    var USD_MYR: Double = 0.0,
+
+    @SerialName("MZN")
+    var USD_MZN: Double = 0.0,
+
+    @SerialName("NAD")
+    var USD_NAD: Double = 0.0,
+
+    @SerialName("NGN")
+    var USD_NGN: Double = 0.0,
+
+    @SerialName("NIO")
+    var USD_NIO: Double = 0.0,
+
+    @SerialName("NOK")
+    var USD_NOK: Double = 0.0,
+
+    @SerialName("NPR")
+    var USD_NPR: Double = 0.0,
+
+    @SerialName("NZD")
+    var USD_NZD: Double = 0.0,
+
+    @SerialName("OMR")
+    var USD_OMR: Double = 0.0,
+
+    @SerialName("PAB")
+    var USD_PAB: Double = 0.0,
+
+    @SerialName("PEN")
+    var USD_PEN: Double = 0.0,
+
+    @SerialName("PGK")
+    var USD_PGK: Double = 0.0,
+
+    @SerialName("PHP")
+    var USD_PHP: Double = 0.0,
+
+    @SerialName("PKR")
+    var USD_PKR: Double = 0.0,
+
+    @SerialName("PLN")
+    var USD_PLN: Double = 0.0,
+
+    @SerialName("PYG")
+    var USD_PYG: Double = 0.0,
+
+    @SerialName("QAR")
+    var USD_QAR: Double = 0.0,
+
+    @SerialName("RON")
+    var USD_RON: Double = 0.0,
+
+    @SerialName("RSD")
+    var USD_RSD: Double = 0.0,
+
+    @SerialName("RUB")
+    var USD_RUB: Double = 0.0,
+
+    @SerialName("RWF")
+    var USD_RWF: Double = 0.0,
+
+    @SerialName("SAR")
+    var USD_SAR: Double = 0.0,
+
+    @SerialName("SBD")
+    var USD_SBD: Double = 0.0,
+
+    @SerialName("SCR")
+    var USD_SCR: Double = 0.0,
+
+    @SerialName("SDG")
+    var USD_SDG: Double = 0.0,
+
+    @SerialName("SEK")
+    var USD_SEK: Double = 0.0,
+
+    @SerialName("SGD")
+    var USD_SGD: Double = 0.0,
+
+    @SerialName("SHP")
+    var USD_SHP: Double = 0.0,
+
+    @SerialName("SLL")
+    var USD_SLL: Double = 0.0,
+
+    @SerialName("SOS")
+    var USD_SOS: Double = 0.0,
+
+    @SerialName("SRD")
+    var USD_SRD: Double = 0.0,
+
+    @SerialName("SSP")
+    var USD_SSP: Double = 0.0,
+
+    @SerialName("STD")
+    var USD_STD: Double = 0.0,
+
+    @SerialName("STN")
+    var USD_STN: Double = 0.0,
+
+    @SerialName("SVC")
+    var USD_SVC: Double = 0.0,
+
+    @SerialName("SYP")
+    var USD_SYP: Double = 0.0,
+
+    @SerialName("SZL")
+    var USD_SZL: Double = 0.0,
+
+    @SerialName("THB")
+    var USD_THB: Double = 0.0,
+
+    @SerialName("TJS")
+    var USD_TJS: Double = 0.0,
+
+    @SerialName("TMT")
+    var USD_TMT: Double = 0.0,
+
+    @SerialName("TND")
+    var USD_TND: Double = 0.0,
+
+    @SerialName("TOP")
+    var USD_TOP: Double = 0.0,
+
+    @SerialName("TRY")
+    var USD_TRY: Double = 0.0,
+
+    @SerialName("TTD")
+    var USD_TTD: Double = 0.0,
+
+    @SerialName("TWD")
+    var USD_TWD: Double = 0.0,
+
+    @SerialName("TZS")
+    var USD_TZS: Double = 0.0,
+
+    @SerialName("UAH")
+    var USD_UAH: Double = 0.0,
+
+    @SerialName("UGX")
+    var USD_UGX: Double = 0.0,
+
+    @SerialName("USD")
+    var USD_USD: Double = 0.0,
+
+    @SerialName("UYU")
+    var USD_UYU: Double = 0.0,
+
+    @SerialName("UZS")
+    var USD_UZS: Double = 0.0,
+
+    @SerialName("VES")
+    var USD_VES: Double = 0.0,
+
+    @SerialName("VND")
+    var USD_VND: Double = 0.0,
+
+    @SerialName("VUV")
+    var USD_VUV: Double = 0.0,
+
+    @SerialName("WST")
+    var USD_WST: Double = 0.0,
+
+    @SerialName("XAF")
+    var USD_XAF: Double = 0.0,
+
+    @SerialName("XAG")
+    var USD_XAG: Double = 0.0,
+
+    @SerialName("XAU")
+    var USD_XAU: Double = 0.0,
+
+    @SerialName("XCD")
+    var USD_XCD: Double = 0.0,
+
+    @SerialName("XDR")
+    var USD_XDR: Double = 0.0,
+
+    @SerialName("XOF")
+    var USD_XOF: Double = 0.0,
+
+    @SerialName("XPD")
+    var USD_XPD: Double = 0.0,
+
+    @SerialName("XPF")
+    var USD_XPF: Double = 0.0,
+
+    @SerialName("XPT")
+    var USD_XPT: Double = 0.0,
+
+    @SerialName("YER")
+    var USD_YER: Double = 0.0,
+
+    @SerialName("ZAR")
+    var USD_ZAR: Double = 0.0,
+
+    @SerialName("ZMW")
+    var USD_ZMW: Double = 0.0,
+
+    @SerialName("ZWL")
+    var USD_ZWL: Double = 0.0,
+) {
     /**
      * Creates a list of Currency objects from the declared fields of this class using reflection
      * to instantiate each object's currency code with the declared field's name. Filters out the
@@ -20,511 +527,4 @@ class ExchangeRates {
         }
 
     override fun toString() = currencies.toString()
-
-    @SerialName("AED")
-    var USD_AED: Double = 0.0
-
-    @SerialName("AFN")
-    var USD_AFN: Double = 0.0
-
-    @SerialName("ALL")
-    var USD_ALL: Double = 0.0
-
-    @SerialName("AMD")
-    var USD_AMD: Double = 0.0
-
-    @SerialName("ANG")
-    var USD_ANG: Double = 0.0
-
-    @SerialName("AOA")
-    var USD_AOA: Double = 0.0
-
-    @SerialName("ARS")
-    var USD_ARS: Double = 0.0
-
-    @SerialName("AUD")
-    var USD_AUD: Double = 0.0
-
-    @SerialName("AWG")
-    var USD_AWG: Double = 0.0
-
-    @SerialName("AZN")
-    var USD_AZN: Double = 0.0
-
-    @SerialName("BAM")
-    var USD_BAM: Double = 0.0
-
-    @SerialName("BBD")
-    var USD_BBD: Double = 0.0
-
-    @SerialName("BDT")
-    var USD_BDT: Double = 0.0
-
-    @SerialName("BGN")
-    var USD_BGN: Double = 0.0
-
-    @SerialName("BHD")
-    var USD_BHD: Double = 0.0
-
-    @SerialName("BIF")
-    var USD_BIF: Double = 0.0
-
-    @SerialName("BMD")
-    var USD_BMD: Double = 0.0
-
-    @SerialName("BND")
-    var USD_BND: Double = 0.0
-
-    @SerialName("BOB")
-    var USD_BOB: Double = 0.0
-
-    @SerialName("BRL")
-    var USD_BRL: Double = 0.0
-
-    @SerialName("BSD")
-    var USD_BSD: Double = 0.0
-
-    @SerialName("BTC")
-    var USD_BTC: Double = 0.0
-
-    @SerialName("BTN")
-    var USD_BTN: Double = 0.0
-
-    @SerialName("BWP")
-    var USD_BWP: Double = 0.0
-
-    @SerialName("BYN")
-    var USD_BYN: Double = 0.0
-
-    @SerialName("BZD")
-    var USD_BZD: Double = 0.0
-
-    @SerialName("CAD")
-    var USD_CAD: Double = 0.0
-
-    @SerialName("CDF")
-    var USD_CDF: Double = 0.0
-
-    @SerialName("CHF")
-    var USD_CHF: Double = 0.0
-
-    @SerialName("CLF")
-    var USD_CLF: Double = 0.0
-
-    @SerialName("CLP")
-    var USD_CLP: Double = 0.0
-
-    @SerialName("CNH")
-    var USD_CNH: Double = 0.0
-
-    @SerialName("CNY")
-    var USD_CNY: Double = 0.0
-
-    @SerialName("COP")
-    var USD_COP: Double = 0.0
-
-    @SerialName("CRC")
-    var USD_CRC: Double = 0.0
-
-    @SerialName("CUC")
-    var USD_CUC: Double = 0.0
-
-    @SerialName("CUP")
-    var USD_CUP: Double = 0.0
-
-    @SerialName("CVE")
-    var USD_CVE: Double = 0.0
-
-    @SerialName("CZK")
-    var USD_CZK: Double = 0.0
-
-    @SerialName("DJF")
-    var USD_DJF: Double = 0.0
-
-    @SerialName("DKK")
-    var USD_DKK: Double = 0.0
-
-    @SerialName("DOP")
-    var USD_DOP: Double = 0.0
-
-    @SerialName("DZD")
-    var USD_DZD: Double = 0.0
-
-    @SerialName("EGP")
-    var USD_EGP: Double = 0.0
-
-    @SerialName("ERN")
-    var USD_ERN: Double = 0.0
-
-    @SerialName("ETB")
-    var USD_ETB: Double = 0.0
-
-    @SerialName("EUR")
-    var USD_EUR: Double = 0.0
-
-    @SerialName("FJD")
-    var USD_FJD: Double = 0.0
-
-    @SerialName("FKP")
-    var USD_FKP: Double = 0.0
-
-    @SerialName("GBP")
-    var USD_GBP: Double = 0.0
-
-    @SerialName("GEL")
-    var USD_GEL: Double = 0.0
-
-    @SerialName("GGP")
-    var USD_GGP: Double = 0.0
-
-    @SerialName("GHS")
-    var USD_GHS: Double = 0.0
-
-    @SerialName("GIP")
-    var USD_GIP: Double = 0.0
-
-    @SerialName("GMD")
-    var USD_GMD: Double = 0.0
-
-    @SerialName("GNF")
-    var USD_GNF: Double = 0.0
-
-    @SerialName("GTQ")
-    var USD_GTQ: Double = 0.0
-
-    @SerialName("GYD")
-    var USD_GYD: Double = 0.0
-
-    @SerialName("HKD")
-    var USD_HKD: Double = 0.0
-
-    @SerialName("HNL")
-    var USD_HNL: Double = 0.0
-
-    @SerialName("HRK")
-    var USD_HRK: Double = 0.0
-
-    @SerialName("HTG")
-    var USD_HTG: Double = 0.0
-
-    @SerialName("HUF")
-    var USD_HUF: Double = 0.0
-
-    @SerialName("IDR")
-    var USD_IDR: Double = 0.0
-
-    @SerialName("ILS")
-    var USD_ILS: Double = 0.0
-
-    @SerialName("IMP")
-    var USD_IMP: Double = 0.0
-
-    @SerialName("INR")
-    var USD_INR: Double = 0.0
-
-    @SerialName("IQD")
-    var USD_IQD: Double = 0.0
-
-    @SerialName("IRR")
-    var USD_IRR: Double = 0.0
-
-    @SerialName("ISK")
-    var USD_ISK: Double = 0.0
-
-    @SerialName("JEP")
-    var USD_JEP: Double = 0.0
-
-    @SerialName("JMD")
-    var USD_JMD: Double = 0.0
-
-    @SerialName("JOD")
-    var USD_JOD: Double = 0.0
-
-    @SerialName("JPY")
-    var USD_JPY: Double = 0.0
-
-    @SerialName("KES")
-    var USD_KES: Double = 0.0
-
-    @SerialName("KGS")
-    var USD_KGS: Double = 0.0
-
-    @SerialName("KHR")
-    var USD_KHR: Double = 0.0
-
-    @SerialName("KMF")
-    var USD_KMF: Double = 0.0
-
-    @SerialName("KPW")
-    var USD_KPW: Double = 0.0
-
-    @SerialName("KRW")
-    var USD_KRW: Double = 0.0
-
-    @SerialName("KWD")
-    var USD_KWD: Double = 0.0
-
-    @SerialName("KYD")
-    var USD_KYD: Double = 0.0
-
-    @SerialName("KZT")
-    var USD_KZT: Double = 0.0
-
-    @SerialName("LAK")
-    var USD_LAK: Double = 0.0
-
-    @SerialName("LBP")
-    var USD_LBP: Double = 0.0
-
-    @SerialName("LKR")
-    var USD_LKR: Double = 0.0
-
-    @SerialName("LRD")
-    var USD_LRD: Double = 0.0
-
-    @SerialName("LSL")
-    var USD_LSL: Double = 0.0
-
-    @SerialName("LYD")
-    var USD_LYD: Double = 0.0
-
-    @SerialName("MAD")
-    var USD_MAD: Double = 0.0
-
-    @SerialName("MDL")
-    var USD_MDL: Double = 0.0
-
-    @SerialName("MGA")
-    var USD_MGA: Double = 0.0
-
-    @SerialName("MKD")
-    var USD_MKD: Double = 0.0
-
-    @SerialName("MMK")
-    var USD_MMK: Double = 0.0
-
-    @SerialName("MNT")
-    var USD_MNT: Double = 0.0
-
-    @SerialName("MOP")
-    var USD_MOP: Double = 0.0
-
-    @SerialName("MRU")
-    var USD_MRU: Double = 0.0
-
-    @SerialName("MUR")
-    var USD_MUR: Double = 0.0
-
-    @SerialName("MVR")
-    var USD_MVR: Double = 0.0
-
-    @SerialName("MWK")
-    var USD_MWK: Double = 0.0
-
-    @SerialName("MXN")
-    var USD_MXN: Double = 0.0
-
-    @SerialName("MYR")
-    var USD_MYR: Double = 0.0
-
-    @SerialName("MZN")
-    var USD_MZN: Double = 0.0
-
-    @SerialName("NAD")
-    var USD_NAD: Double = 0.0
-
-    @SerialName("NGN")
-    var USD_NGN: Double = 0.0
-
-    @SerialName("NIO")
-    var USD_NIO: Double = 0.0
-
-    @SerialName("NOK")
-    var USD_NOK: Double = 0.0
-
-    @SerialName("NPR")
-    var USD_NPR: Double = 0.0
-
-    @SerialName("NZD")
-    var USD_NZD: Double = 0.0
-
-    @SerialName("OMR")
-    var USD_OMR: Double = 0.0
-
-    @SerialName("PAB")
-    var USD_PAB: Double = 0.0
-
-    @SerialName("PEN")
-    var USD_PEN: Double = 0.0
-
-    @SerialName("PGK")
-    var USD_PGK: Double = 0.0
-
-    @SerialName("PHP")
-    var USD_PHP: Double = 0.0
-
-    @SerialName("PKR")
-    var USD_PKR: Double = 0.0
-
-    @SerialName("PLN")
-    var USD_PLN: Double = 0.0
-
-    @SerialName("PYG")
-    var USD_PYG: Double = 0.0
-
-    @SerialName("QAR")
-    var USD_QAR: Double = 0.0
-
-    @SerialName("RON")
-    var USD_RON: Double = 0.0
-
-    @SerialName("RSD")
-    var USD_RSD: Double = 0.0
-
-    @SerialName("RUB")
-    var USD_RUB: Double = 0.0
-
-    @SerialName("RWF")
-    var USD_RWF: Double = 0.0
-
-    @SerialName("SAR")
-    var USD_SAR: Double = 0.0
-
-    @SerialName("SBD")
-    var USD_SBD: Double = 0.0
-
-    @SerialName("SCR")
-    var USD_SCR: Double = 0.0
-
-    @SerialName("SDG")
-    var USD_SDG: Double = 0.0
-
-    @SerialName("SEK")
-    var USD_SEK: Double = 0.0
-
-    @SerialName("SGD")
-    var USD_SGD: Double = 0.0
-
-    @SerialName("SHP")
-    var USD_SHP: Double = 0.0
-
-    @SerialName("SLL")
-    var USD_SLL: Double = 0.0
-
-    @SerialName("SOS")
-    var USD_SOS: Double = 0.0
-
-    @SerialName("SRD")
-    var USD_SRD: Double = 0.0
-
-    @SerialName("SSP")
-    var USD_SSP: Double = 0.0
-
-    @SerialName("STD")
-    var USD_STD: Double = 0.0
-
-    @SerialName("STN")
-    var USD_STN: Double = 0.0
-
-    @SerialName("SVC")
-    var USD_SVC: Double = 0.0
-
-    @SerialName("SYP")
-    var USD_SYP: Double = 0.0
-
-    @SerialName("SZL")
-    var USD_SZL: Double = 0.0
-
-    @SerialName("THB")
-    var USD_THB: Double = 0.0
-
-    @SerialName("TJS")
-    var USD_TJS: Double = 0.0
-
-    @SerialName("TMT")
-    var USD_TMT: Double = 0.0
-
-    @SerialName("TND")
-    var USD_TND: Double = 0.0
-
-    @SerialName("TOP")
-    var USD_TOP: Double = 0.0
-
-    @SerialName("TRY")
-    var USD_TRY: Double = 0.0
-
-    @SerialName("TTD")
-    var USD_TTD: Double = 0.0
-
-    @SerialName("TWD")
-    var USD_TWD: Double = 0.0
-
-    @SerialName("TZS")
-    var USD_TZS: Double = 0.0
-
-    @SerialName("UAH")
-    var USD_UAH: Double = 0.0
-
-    @SerialName("UGX")
-    var USD_UGX: Double = 0.0
-
-    @SerialName("USD")
-    var USD_USD: Double = 0.0
-
-    @SerialName("UYU")
-    var USD_UYU: Double = 0.0
-
-    @SerialName("UZS")
-    var USD_UZS: Double = 0.0
-
-    @SerialName("VES")
-    var USD_VES: Double = 0.0
-
-    @SerialName("VND")
-    var USD_VND: Double = 0.0
-
-    @SerialName("VUV")
-    var USD_VUV: Double = 0.0
-
-    @SerialName("WST")
-    var USD_WST: Double = 0.0
-
-    @SerialName("XAF")
-    var USD_XAF: Double = 0.0
-
-    @SerialName("XAG")
-    var USD_XAG: Double = 0.0
-
-    @SerialName("XAU")
-    var USD_XAU: Double = 0.0
-
-    @SerialName("XCD")
-    var USD_XCD: Double = 0.0
-
-    @SerialName("XDR")
-    var USD_XDR: Double = 0.0
-
-    @SerialName("XOF")
-    var USD_XOF: Double = 0.0
-
-    @SerialName("XPD")
-    var USD_XPD: Double = 0.0
-
-    @SerialName("XPF")
-    var USD_XPF: Double = 0.0
-
-    @SerialName("XPT")
-    var USD_XPT: Double = 0.0
-
-    @SerialName("YER")
-    var USD_YER: Double = 0.0
-
-    @SerialName("ZAR")
-    var USD_ZAR: Double = 0.0
-
-    @SerialName("ZMW")
-    var USD_ZMW: Double = 0.0
-
-    @SerialName("ZWL")
-    var USD_ZWL: Double = 0.0
 }
