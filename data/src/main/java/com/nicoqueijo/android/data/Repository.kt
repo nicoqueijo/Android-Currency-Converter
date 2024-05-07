8f3dfe4c33c371bb5e5c2abe60e5f65dabccb8d2
Nicolas Queijo <queijonicolas@gmail.com>
2024-05-07 12:23:22 -0400

    Added dispatcher to the Repository.

--- data/src/main/java/com/nicoqueijo/android/data/Repository.kt
+++ data/src/main/java/com/nicoqueijo/android/data/Repository.kt
@@ -1,12 +1,15 @@
 package com.nicoqueijo.android.data
 
+import com.nicoqueijo.android.core.di.IODispatcher
 import com.nicoqueijo.android.network.KtorClient
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val ktorClient: KtorClient,
     private val currencyDao: CurrencyDao,
-    private val dataStoreManager: DataStoreManager
+    private val dataStoreManager: DataStoreManager,
+    @IODispatcher private val dispatcher: CoroutineDispatcher
 ){
     suspend fun getCurrencies() = ktorClient.getExchangeRates().exchangeRates?.currencies
 }
