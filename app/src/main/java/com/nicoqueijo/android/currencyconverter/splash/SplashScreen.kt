44d860f122be4ee3ffc940dd3f966538c0909edf
Nicolas Queijo <queijonicolas@gmail.com>
2024-05-09 12:47:43 -0400

    Added Splash screen Composable.

---
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/splash/SplashScreen.kt
@@ -0,0 +1,58 @@
+package com.nicoqueijo.android.currencyconverter.splash
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.nicoqueijo.android.currencyconverter.R
+import com.nicoqueijo.android.currencyconverter.ui.theme.AndroidCurrencyConverterTheme
+import com.nicoqueijo.android.ui.DarkLightPreviews
+import com.nicoqueijo.android.ui.L
+import com.nicoqueijo.android.ui.S
+import com.nicoqueijo.android.ui.XL
+import com.nicoqueijo.android.ui.XXXS
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    AndroidCurrencyConverterTheme {
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier.padding(horizontal = L, vertical = XL),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(space = S)
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(size = 125.dp)
+                        .padding(all = XXXS),
+                    strokeWidth = 8.dp
+                )
+                Text(
+                    text = stringResource(id = R.string.splash_title),
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@DarkLightPreviews
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
