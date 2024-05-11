69358f04d4488b3884c9d633d9e6587ad295b323
Nicolas Queijo <queijonicolas@gmail.com>
2024-05-10 21:10:42 -0400

    Remove unneeded code.

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/MainActivity.kt
@@ -18,33 +18,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.nicoqueijo.android.currencyconverter.splash.SplashScreen
-import com.nicoqueijo.android.data.CurrencyRepository
-import com.nicoqueijo.android.data.Repository
 import com.nicoqueijo.android.ui.AndroidCurrencyConverterTheme
 import com.nicoqueijo.android.ui.DarkLightPreviews
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.runBlocking
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var currencyRepository: Repository
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        var result = "Uninitialized"
-
-        runBlocking {
-            currencyRepository.getExchangeRates().onSuccess {
-                result = it.exchangeRates!!.currencies[60].toString()
-            }.onFailure {
-                result = it.message.toString()
-            }
-        }
-
         setContent {
             val navController = rememberNavController()
             AndroidCurrencyConverterTheme {
