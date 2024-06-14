f6b1831f49efb70c742b6b55246c6aae7bb54e99
Nicolas Queijo <queijonicolas@gmail.com>
2024-06-14 19:56:47 -0400

    Fixed issue where empty state would appear in the screen for a tiny moment.

--- feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/ConvertCurrencyViewModel.kt
+++ feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/ConvertCurrencyViewModel.kt
@@ -7,8 +7,11 @@ import com.nicoqueijo.android.core.di.DefaultDispatcher
 import com.nicoqueijo.android.data.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -23,13 +26,12 @@ class ConvertCurrencyViewModel @Inject constructor(
     val uiState = _uiState.asStateFlow()
 
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(context = dispatcher) {
             _uiState.value = _uiState.value.copy(
                 selectedCurrencies = repository.getSelectedCurrencies()
             )
         }
     }
-
 }
 
 data class ConvertCurrencyUiState(
