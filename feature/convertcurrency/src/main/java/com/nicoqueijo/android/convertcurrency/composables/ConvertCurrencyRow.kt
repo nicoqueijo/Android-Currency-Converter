0b259f86dca4793fa0ffb5f21e558c124344237f
Nicolas Queijo <queijonicolas@gmail.com>
2024-06-19 23:16:06 -0400

    Added hint and focus to the composable row.

--- feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/composables/ConvertCurrencyRow.kt
+++ feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/composables/ConvertCurrencyRow.kt
@@ -1,6 +1,7 @@
 package com.nicoqueijo.android.convertcurrency.composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -31,20 +32,24 @@ import com.nicoqueijo.android.ui.XS
 import com.nicoqueijo.android.ui.XXS
 import com.nicoqueijo.android.ui.XXXS
 import com.nicoqueijo.android.ui.extensions.getDrawableResourceByName
-import java.math.BigDecimal
 
 @Composable
 fun ConvertCurrencyRow(
     modifier: Modifier = Modifier,
     state: Currency,
 ) {
-    Surface(
-        color = if (state.isFocused) Color.Gray else MaterialTheme.colorScheme.surface
-    ) {
+    Surface(modifier = modifier) {
         Row(
-            modifier = modifier
+            modifier = Modifier
+                .background(
+                    color = if (state.isFocused) {
+                        Color.Gray
+                    } else {
+                        MaterialTheme.colorScheme.surface
+                    }
+                )
                 .fillMaxWidth()
-                .height(56.dp)
+                .height(height = 56.dp)
                 .padding(
                     horizontal = XS,
                     vertical = XXS,
@@ -54,15 +59,17 @@ fun ConvertCurrencyRow(
             Image(
                 modifier = Modifier
                     .padding(vertical = XXXS)
-                    .clip(shape = RoundedCornerShape(2.dp)),
+                    .clip(shape = RoundedCornerShape(size = 2.dp)),
                 contentDescription = null,
                 painter = painterResource(
-                    id = LocalContext.current.getDrawableResourceByName(name = state.currencyCode.lowercase())
+                    id = LocalContext.current.getDrawableResourceByName(
+                        name = state.currencyCode.lowercase()
+                    )
                 )
             )
             Spacer(
                 modifier = Modifier
-                    .width(S)
+                    .width(width = S)
                     .fillMaxHeight()
             )
             Text(
@@ -72,18 +79,27 @@ fun ConvertCurrencyRow(
             )
             Spacer(
                 modifier = Modifier
-                    .width(S)
+                    .width(width = S)
                     .fillMaxHeight()
             )
             // TODO: Add the fading edge to the start of this Text
             // TODO: Add a hint (conversion.hint) to this Text when it's empty
             Text(
-                modifier = Modifier.weight(1f),
-                text = state.conversion.valueAsText,
+                modifier = Modifier.weight(
+                    weight = 1f,
+                ),
+                text = state.conversion.valueAsText.ifEmpty {
+                    state.conversion.hint
+                },
+                color = if (state.conversion.valueAsText.isEmpty()) {
+                    Color.Gray
+                } else {
+                    Color.Unspecified
+                },
                 textAlign = TextAlign.End,
                 maxLines = 1,
             )
-            // TODO: Add blinking cursor
+            // TODO: Add blinking cursor when focused
         }
     }
 }
@@ -95,7 +111,51 @@ fun ConvertCurrencyRowPreview() {
         currencyCode = "USD_CAD",
         exchangeRate = 1.36175,
     ).apply {
-        conversion.value = BigDecimal("197759825546354669.4458")
+        conversion.valueAsString = "12345.6789"
+    }
+    AndroidCurrencyConverterTheme {
+        ConvertCurrencyRow(state = currency)
+    }
+}
+
+@DarkLightPreviews
+@Composable
+fun ConvertCurrencyRowFocusedPreview() {
+    val currency = Currency(
+        currencyCode = "USD_CAD",
+        exchangeRate = 1.36175,
+    ).apply {
+        conversion.valueAsString = "12345.6789"
+        isFocused = true
+    }
+    AndroidCurrencyConverterTheme {
+        ConvertCurrencyRow(state = currency)
+    }
+}
+
+@DarkLightPreviews
+@Composable
+fun ConvertCurrencyRowHintPreview() {
+    val currency = Currency(
+        currencyCode = "USD_CAD",
+        exchangeRate = 1.36175,
+    ).apply {
+        conversion.hint = "1"
+    }
+    AndroidCurrencyConverterTheme {
+        ConvertCurrencyRow(state = currency)
+    }
+}
+
+@DarkLightPreviews
+@Composable
+fun ConvertCurrencyRowFocusedHintPreview() {
+    val currency = Currency(
+        currencyCode = "USD_CAD",
+        exchangeRate = 1.36175,
+    ).apply {
+        isFocused = true
+        conversion.hint = "1"
     }
     AndroidCurrencyConverterTheme {
         ConvertCurrencyRow(state = currency)
--- feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/composables/ConvertCurrencyScreen.kt
+++ feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/composables/ConvertCurrencyScreen.kt
@@ -3,6 +3,7 @@
 package com.nicoqueijo.android.convertcurrency.composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -27,6 +28,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -72,7 +74,6 @@ fun ConvertCurrency(
         modifier = modifier.fillMaxSize(),
     ) {
         Scaffold(
-            modifier = modifier,
             topBar = {
                 Column {
                     TopAppBar(
