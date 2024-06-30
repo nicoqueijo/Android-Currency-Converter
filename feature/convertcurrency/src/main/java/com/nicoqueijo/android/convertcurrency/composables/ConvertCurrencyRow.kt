48dde179eb79d53d8c1d3fd1e26be7e0312a9a74
Nicolas Queijo <queijonicolas@gmail.com>
2024-06-30 15:15:32 -0400

    Removed unused imports.

--- feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/composables/ConvertCurrencyRow.kt
+++ feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/composables/ConvertCurrencyRow.kt
@@ -1,9 +1,5 @@
 package com.nicoqueijo.android.convertcurrency.composables
 
-import android.content.Context
-import android.os.VibrationEffect
-import android.os.Vibrator
-import android.widget.Toast
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
@@ -29,9 +25,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
