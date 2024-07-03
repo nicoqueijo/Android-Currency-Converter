ecb67e768b482b2bcc21d123d7a1b32de309324b
Nicolas Queijo <queijonicolas@gmail.com>
2024-07-03 14:45:42 -0400

    Organized code in the use case.

--- feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/usecases/UnselectCurrencyUseCase.kt
+++ feature/convertcurrency/src/main/java/com/nicoqueijo/android/convertcurrency/usecases/UnselectCurrencyUseCase.kt
@@ -11,30 +11,16 @@ class UnselectCurrencyUseCase(
 
     suspend operator fun invoke(currency: Currency) {
         val selectedCurrencies = repository.getSelectedCurrencies().first()
-
-        val currencyToUnselect = try {
-            selectedCurrencies.first { it.currencyCode == currency.currencyCode }
-        } catch (e: NoSuchElementException) {
-            return
+        val currencyToUnselect = selectedCurrencies.first { selectedCurrency ->
+            selectedCurrency.currencyCode == currency.currencyCode
         }
-
         for (i in selectedCurrencies.count() - 1 downTo  currencyToUnselect.position + 1) {
             selectedCurrencies[i].position = --selectedCurrencies[i].position
         }
-
         currencyToUnselect.apply {
             isSelected = false
             position = Position.INVALID.value
         }
-
         repository.upsertCurrencies(selectedCurrencies)
-
-
-        /**
-         * 1. Unselect the currency
-         * 2. Change the position to invalid
-         * 3. Adjust the positions of the currencies below this
-         *      (decrement them by 1)
-         */
     }
 }
